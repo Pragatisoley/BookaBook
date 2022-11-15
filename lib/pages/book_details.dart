91c09d970985df49e7ea2842763064c0e49dfcae
Akshata <akshatagawande2002@gmail.com>
2022-11-15 11:35:48 +0530

    details

---
+++ lib/pages/book_details.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BookDetails extends StatefulWidget {
+  final product_detail_name;
+  final product_detail_new_price;
+  final product_detail_old_price;
+  final product_detail_picture;
+
+
+  ProductDetails({
+    this.product_detail_name,
+    this.product_detail_new_price,
+    this.product_detail_old_price,
+    this.product_detail_picture
+  });
